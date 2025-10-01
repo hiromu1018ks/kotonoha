a7e585866bc2a77dcab0db74475b510878cc7d7b
hiromu1018ks <hiromu1018ks@gmail.com>
2025-10-01 21:43:12 +0900

    feat: next.config.tsのヘッダー設定をコメントアウト
    ヘッダー設定を無効化し、プロダクション環境でのContent-Security-Policyの適用を一時的に停止しました。

--- next.config.ts
+++ next.config.ts
@@ -3,20 +3,20 @@ import type { NextConfig } from "next";
 const isProd = process.env.NODE_ENV === "production";
 
 const nextConfig: NextConfig = {
-  headers: async () =>
-    isProd
-      ? [
-          {
-            source: "/(.*)",
-            headers: [
-              {
-                key: "Content-Security-Policy",
-                value: "script-src 'self'; object-src 'none'",
-              },
-            ],
-          },
-        ]
-      : [],
+  // headers: async () =>
+  //   isProd
+  //     ? [
+  //         {
+  //           source: "/(.*)",
+  //           headers: [
+  //             {
+  //               key: "Content-Security-Policy",
+  //               value: "script-src 'self'; object-src 'none'",
+  //             },
+  //           ],
+  //         },
+  //       ]
+  //     : [],
 };
 
 export default nextConfig;
